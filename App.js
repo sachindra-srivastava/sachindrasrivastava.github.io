9873d3be261f636c0427849901452d6cce860b48
sachindra <sachindra.1612@gmail.com>
2017-03-09 18:36:20 +0530

    changes

--- App.js
+++ App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Sach-kingdom</h2>
         </div>
         <p className="App-intro">
-          To get started, edit the <code>src/App.js</code> and save to reload it.
+          To get started, edit the <code>src/App.js</code> and save to reload it!Goood luck.
         </p>
       </div>
     );
