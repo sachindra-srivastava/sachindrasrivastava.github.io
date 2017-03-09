126586bc789797865e1272b2f62ecf8aef03ee03
sachindra <sachindra.1612@gmail.com>
2017-03-09 19:23:42 +0530

    new file

--- src/index.js
+++ src/index.js
@@ -3,18 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-class Application extends React.Component {
-  render() {
-    return <div>
-      <h1>Hello sach, ES6 and React 0.13!</h1>
-      <p>
-        More info <a href="https://github.com/bradleyboy/codepen-react" target="_blank">here</a>.
-      </p>
-    </div>;
-  }
-}
-
-/*
- * Render the above component into the div#app
- */
-React.render(<Application />, document.getElementById('app'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
